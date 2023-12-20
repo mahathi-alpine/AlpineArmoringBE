4a4f7ce8cee3c698d2ebc66c949334d1809eb17b
Stefan <petkovics93@yahoo.com>
2023-12-20 09:20:48 +0100

    cloudinary

--- config/plugins.js
+++ config/plugins.js
@@ -1,5 +1,19 @@
 module.exports = {
-    'drag-drop-content-types': {
-      enabled: true
-    }
-  }
+  'drag-drop-content-types': {
+    enabled: true
+  },
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+  },
+}
