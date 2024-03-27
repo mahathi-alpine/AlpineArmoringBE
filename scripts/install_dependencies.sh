1aaa4bcd8b05c8656bb3cc05f3133402c5e5a106
Joseph Spielman <106697709+cloud303-jspielman@users.noreply.github.com>
2024-03-27 13:45:01 -0600

    Updated scripts for before and after install

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-# Navigate to the project directory
-cd /var/www/html/myapp
+# Install NVM
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash 
+source ~/.bashrc
+source ~/.nvm/nvm.sh
 
-# Install project dependencies
-yarn install
+# Install or update Node.js LTS using NVM within the specified range
+nvm install 18.17.0
 
+# Install Yarn
+npm install -g yarn
--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -3,6 +3,14 @@
 # Navigate to the project directory
 cd /var/www/html/myapp
 
+# Fix node version error - hardcoded to 18.17.0 for now
+source ~/.nvm/nvm.sh
+
+nvm use 18.17.0
+
+# Install project dependencies
+yarn install
+
 # Get enviornment variables from parameter store
 env_vars=$(aws ssm get-parameter --name "env_vars" --query "Parameter.Value" --output text)
 
@@ -23,16 +31,11 @@ else
     exit 1
 fi
 
-# Fix node version error - hardcoded to 18.17.0 for now
-source ~/.nvm/nvm.sh
-
-nvm use 18.17.0
-
 # Build the application
 yarn build
 
 # Create logging directory
-mkdir /home/ubuntu/logs && touch /home/ubuntu/logs/strapi.log
+mkdir -p /home/ubuntu/logs && touch /home/ubuntu/logs/strapi.log
 
 # Run the application in the background using Yarn
 nohup yarn start > /home/ubuntu/logs/strapi.log 2>&1 &
