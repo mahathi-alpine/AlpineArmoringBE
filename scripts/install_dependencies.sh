bb02c390f5abf73c92bc073ac5f28999f64e238e
Joseph Spielman <106697709+cloud303-jspielman@users.noreply.github.com>
2024-04-08 14:41:48 -0600

    Fix NVM path error

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,7 +2,15 @@
 
 # Install NVM
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash 
+
 source ~/.bashrc
+
+# Load NVM to avoid pathing error
+export NVM_DIR="$HOME/.nvm"
+
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
 # Install or update Node.js LTS using NVM within the specified range
 nvm install 18.17.0
