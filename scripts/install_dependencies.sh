e9f5c1a801e5273a1042fe0c546abd27f781c0f3
Joseph Spielman <106697709+cloud303-jspielman@users.noreply.github.com>
2024-04-08 14:32:38 -0600

    Updating install_dependencies.sh, removing user data

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+# Install NVM
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash 
+source ~/.bashrc
+
+# Install or update Node.js LTS using NVM within the specified range
+nvm install 18.17.0
+
+# Install Yarn
+npm install -g yarn
+
+# Install pm2
+npm install -g pm2
