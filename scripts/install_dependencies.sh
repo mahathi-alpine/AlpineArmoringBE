92d0bae125220b88c9c38b80797b7cda2857c90c
Joseph Spielman <106697709+cloud303-jspielman@users.noreply.github.com>
2024-04-08 14:17:21 -0600

    Clear out install_dependencies script (moved to ec2 user data)

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,15 +1,1 @@
 #!/bin/bash
-
-# Install NVM
-curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash 
-source ~/.bashrc
-source ~/.nvm/nvm.sh
-
-# Install or update Node.js LTS using NVM within the specified range
-nvm install 18.17.0
-
-# Install Yarn
-npm install -g yarn
-
-# Install pm2
-npm install -g pm2
