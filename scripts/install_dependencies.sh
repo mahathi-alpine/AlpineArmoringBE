ad4b07db8014dd6e806ae1e44913bd11e4f3d504
Joseph Spielman <106697709+cloud303-jspielman@users.noreply.github.com>
2024-04-09 17:30:56 -0600

    Fix install_dependencies.sh script

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
+# Install Yarn
+npm install --global yarn
+
+# Install pm2
+npm install --global pm2
+
 # Install NVM
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash 
+
 source ~/.bashrc
-source ~/.nvm/nvm.sh
+
+# Load NVM to avoid pathing error
+export NVM_DIR="$HOME/.nvm"
+
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
 # Install or update Node.js LTS using NVM within the specified range
 nvm install 18.17.0
 
-# Install Yarn
-npm install -g yarn
-
-# Install pm2
-npm install -g pm2
