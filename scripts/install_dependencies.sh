188fa45fc274aa280feb4d0109fcf28bcecd4445
Joseph Spielman <106697709+cloud303-jspielman@users.noreply.github.com>
2024-04-09 21:32:02 -0600

    put all commands under one command for ubuntu user

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -7,13 +7,5 @@ npm install --global yarn
 npm install --global pm2
 
 # Install NVM
-su - ubuntu -c 'curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash'
-
-su - ubuntu -c 'source ~/.bashrc'
-
-# Load NVM to avoid pathing error
-su - ubuntu -c 'source ~/.bashrc && export NVM_DIR="$HOME/.nvm" && [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" && [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"'
-
-# Install or update Node.js LTS using NVM within the specified range
-su - ubuntu -c 'source ~/.bashrc && nvm install 18.17.0'
+su - ubuntu -c 'curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash && source ~/.bashrc && export NVM_DIR="/home/ubuntu/.nvm" && [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" && [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" && nvm install 18.17.0'
 
