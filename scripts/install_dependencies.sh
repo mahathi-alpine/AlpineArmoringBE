493a37cc0ab876e990c2f5799765258c856dc078
Joseph Spielman <106697709+cloud303-jspielman@users.noreply.github.com>
2024-04-09 21:07:28 -0600

    update install_dependencies to run as ubuntu user

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -7,17 +7,13 @@ npm install --global yarn
 npm install --global pm2
 
 # Install NVM
-curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash 
+su - ubuntu -c 'curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash'
 
-source ~/.bashrc
+su - ubuntu -c 'source ~/.bashrc'
 
 # Load NVM to avoid pathing error
-export NVM_DIR="$HOME/.nvm"
-
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+su - ubuntu -c 'export NVM_DIR="$HOME/.nvm" && [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" && [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"'
 
 # Install or update Node.js LTS using NVM within the specified range
-nvm install 18.17.0
+su - ubuntu -c 'nvm install 18.17.0'
 
