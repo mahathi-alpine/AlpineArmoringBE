745aa303f8e426c18067f7fb8309b18c2717b422
Joseph Spielman <106697709+cloud303-jspielman@users.noreply.github.com>
2024-04-10 14:33:12 -0600

    Update s3 plugin with private acl defined

--- config/middlewares.js
+++ config/middlewares.js
@@ -7,8 +7,8 @@ module.exports = [
         useDefaults: true,
         directives: {
           'connect-src': ["'self'", 'https:'],
-          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com', 'market-assets.strapi.io', 'strapi.io', 'alpine-backend-992382787275.s3.us-east-1.amazonaws.com'],
-          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com', 'market-assets.strapi.io', 'strapi.io', 'alpine-backend-992382787275.s3.us-east-1.amazonaws.com'],
+          'img-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'strapi.io', 'alpine-backend-992382787275.s3.us-east-1.amazonaws.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'strapi.io', 'alpine-backend-992382787275.s3.us-east-1.amazonaws.com'],
           upgradeInsecureRequests: null,
         },
       },
