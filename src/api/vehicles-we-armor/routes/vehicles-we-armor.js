07dffc8977dd908c5d939c3186a08853a80625c9
Stefan <petkovics93@yahoo.com>
2024-04-16 14:35:48 +0200

    vwa middleware

--- src/api/vehicles-we-armor/routes/vehicles-we-armor.js
+++ src/api/vehicles-we-armor/routes/vehicles-we-armor.js
@@ -6,13 +6,14 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::vehicles-we-armor.vehicles-we-armor', {
-    config: {
-        find: {
-            middlewares: ['api::vehicles-we-armor.vehicles-we-armor-populate']
-        }, findOne: {
-            middlewares: ['api::vehicles-we-armor.vehicles-we-armor-populate']
-        }
-    }
-});
+// module.exports = createCoreRouter('api::vehicles-we-armor.vehicles-we-armor', {
+//     config: {
+//         find: {
+//             middlewares: ['api::vehicles-we-armor.vehicles-we-armor-populate']
+//         }, findOne: {
+//             middlewares: ['api::vehicles-we-armor.vehicles-we-armor-populate']
+//         }
+//     }
+// });
 
+module.exports = createCoreRouter('api::vehicles-we-armor.vehicles-we-armor');
