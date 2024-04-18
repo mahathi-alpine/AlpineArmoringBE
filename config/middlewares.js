8b1b8542b102ee115245d09fbb28fd552af77237
Stefan <petkovics93@yahoo.com>
2024-04-18 13:28:22 +0200

    cdn images

--- config/middlewares.js
+++ config/middlewares.js
@@ -7,8 +7,8 @@ module.exports = [
         useDefaults: true,
         directives: {
           'connect-src': ["'self'", 'https:'],
-          'img-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'strapi.io', 'alpine-backend-992382787275.s3.us-east-1.amazonaws.com'],
-          'media-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'strapi.io', 'alpine-backend-992382787275.s3.us-east-1.amazonaws.com'],
+          'img-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'strapi.io', 'alpine-backend-992382787275.s3.us-east-1.amazonaws.com', 'd102sycao8uwt8.cloudfront.net'],
+          'media-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'strapi.io', 'alpine-backend-992382787275.s3.us-east-1.amazonaws.com', 'd102sycao8uwt8.cloudfront.net'],
           upgradeInsecureRequests: null,
         },
       },
